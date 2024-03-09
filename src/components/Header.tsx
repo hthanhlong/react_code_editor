8a008f910cbe1af913d5d73610a4679f0609544c
LongHoang <longhoang@gmail.com>
2024-03-08 16:40:06 -0800

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-black h-20 text-cyan-100">
+      <span className="absolute top-6 left-5">CodePenClone</span>
+    </header>
+  )
+}
+
+export default Header
